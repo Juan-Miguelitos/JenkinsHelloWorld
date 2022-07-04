14cdfedba37cff57eb52350fc5852ad7ce88fc3f
Juan-Miguelitos <73351499+Juan-Miguelitos@users.noreply.github.com>
2022-07-04 16:42:44 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,5 @@
 public class HelloWorld {
         public static void main(String[] args){
-                System.out.println("-----***** Made another new changes *****-----")};
+                System.out.println("-----***** Made another new changes *****-----");
+        }
 }
